be1e11b60308935c7e40830bce7060d124d47241
tanvir321110 <tanvirahmed321110@gmail.com>
2024-11-26 16:29:49 +0600

    spinner added

--- Js/utlitis.js
+++ Js/utlitis.js
@@ -3,3 +3,18 @@ function pageF(url) {
         window.location.href = url;
     }, 1000);
 }
+
+
+function showContentHideSpinner(spinner, data, delay) {
+    setTimeout(() => {
+        document.querySelector(spinner).style.display = 'none'
+        document.querySelector(data).style.display = 'block'
+    }, delay);
+}
+
+window.onload = function () {
+    showContentHideSpinner('.spinner', '.body-card', 2000)
+}
+
+
+export default showContentHideSpinner;
